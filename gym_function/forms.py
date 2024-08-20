d616b383a9163b7f706d9e205a7667bac2683b71
francis Valenzuela Ganapin <66492676+francisganapin@users.noreply.github.com>
2024-08-20 15:58:41 +0800

    8/20/2024

--- gym_function/forms.py
+++ gym_function/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import gym_members,gym_item,gym_trainor
+from django.core.exceptions import ValidationError
 
 class RegisterFormMember(forms.ModelForm):
     expiry = forms.DateField(
@@ -11,7 +12,15 @@ class RegisterFormMember(forms.ModelForm):
     class Meta:
         model = gym_members
         fields = ['id_card', 'expiry', 'membership', 'first_name', 'last_name', 'phone_number', 'address']
-
+        
+    def clean_id_card(self):
+        id_card = self.cleaned_data.get('id_card')
+        if gym_members.objects.filter(id_card=id_card).exists():
+            raise ValidationError(f'This ID {id_card} card already exists')
+        return id_card
+    
+    
+    
 class RegisterFormTrainor(forms.ModelForm):
       
       class Meta:
